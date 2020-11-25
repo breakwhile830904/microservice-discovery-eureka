ddc16d234cfe1f3a7d8b40216ead6e3d51189b19
wei-qiang <wei-qiang@HT201407070120A.neusoft.internal>
2020-11-25 14:21:51 +0800

    1421

--- microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * 使用Eureka做服务发现.
  * 2020-11-25
- * @author 周立12345
+ * @author 周立123456
  */
 @SpringBootApplication
 @EnableEurekaServer
